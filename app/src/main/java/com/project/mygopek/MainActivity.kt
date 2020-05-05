4a10be812c30dac48d20bfd7144b257bbd7c2ce5
cowokmalas <fahmyfauzii@gmail.com>
2020-05-05 23:21:54 +0700

    Ini adalah Project sederhana tablayout dengan icon dan viewpager

---
+++ app/src/main/java/com/project/mygopek/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.project.mygopek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.mygopek.adapter.MyAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        vp_main.adapter=MyAdapter(supportFragmentManager)
+        tab_layout.setupWithViewPager(vp_main)
+        tab_layout.getTabAt(0)!!.setIcon(R.drawable.ic_home_white_24dp)
+        tab_layout.getTabAt(1)!!.setIcon(R.drawable.ic_widgets_white_24dp)
+        tab_layout.getTabAt(2)!!.setIcon(R.drawable.ic_list_white_24dp)
+
+
+    }
+}
---
+++ app/src/main/java/com/project/mygopek/adapter/MyAdapter.kt
@@ -0,0 +1,30 @@
+package com.project.mygopek.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.project.mygopek.R
+import com.project.mygopek.fragment.FirstFragment
+import com.project.mygopek.fragment.LastFragment
+import com.project.mygopek.fragment.SeccondFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MyAdapter(fm: FragmentManager):FragmentPagerAdapter(fm){
+
+    private val pages=listOf(
+    FirstFragment(),
+    SeccondFragment(),
+      LastFragment()
+ )
+    override fun getItem(position: Int): Fragment {
+    return pages[position]
+ }
+
+    override fun getCount(): Int {
+    return pages.size
+ }
+
+}
+
+
+
