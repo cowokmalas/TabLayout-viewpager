4068f96c3177de5e4d7c2adfac7de5a9e453244a
jejemo <jpistomo@gmail.com>
2020-05-09 17:13:09 +0700

    Clean

--- app/src/main/java/com/project/mygopek/fragment/LastFragment.kt
+++ app/src/main/java/com/project/mygopek/fragment/LastFragment.kt
@@ -7,33 +7,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.project.mygopek.DataMoney
-import com.project.mygopek.Money
+import com.project.mygopek.data.Money
 
 import com.project.mygopek.R
+import com.project.mygopek.adapter.ListAdapter
+import com.project.mygopek.data.DataMoney
+import kotlinx.android.synthetic.main.fragment_last.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class LastFragment : Fragment() {
 
-    private lateinit var listview: RecyclerView
-    private var list:ArrayList<Money> = ArrayList()
+    private lateinit var mAdapter: ListAdapter
+
+    private var mList: ArrayList<Money> = ArrayList()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_last, container, false)
-
-        listview=view.findViewById(R.id.rv_list)
-        listview.layoutManager= LinearLayoutManager(activity)
-        list.addAll(DataMoney.listData)
-        listview.setHasFixedSize(true)
-        listview.adapter=com.project.mygopek.adapter.ListAdapter(list)
-        return view
+        // only for inflate a view
+        return inflater.inflate(R.layout.fragment_last, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        setupData()
+        setupAdapter()
+    }
+
+    private fun setupData() {
+        mList.clear()
+        mList.addAll(DataMoney.listData)
+    }
+
+    private fun setupAdapter() {
+        mAdapter = ListAdapter(mList)
+        mAdapter.notifyDataSetChanged()
+        rv_list.apply {
+            adapter = mAdapter
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
+    }
 }
