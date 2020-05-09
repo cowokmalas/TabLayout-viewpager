50724e41c0c514bc987c02bfa106788bc72ef967
jejemo <jpistomo@gmail.com>
2020-05-09 17:11:30 +0700

    Improve

--- app/src/main/java/com/project/mygopek/MainActivity.kt
+++ app/src/main/java/com/project/mygopek/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.project.mygopek
 
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.project.mygopek.adapter.MyAdapter
+import com.project.mygopek.adapter.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        vp_main.adapter=MyAdapter(supportFragmentManager)
+        vp_main.adapter = ViewPagerAdapter(supportFragmentManager)
         tab_layout.setupWithViewPager(vp_main)
         tab_layout.getTabAt(0)!!.setIcon(R.drawable.ic_home_white_24dp)
         tab_layout.getTabAt(1)!!.setIcon(R.drawable.ic_widgets_white_24dp)
         tab_layout.getTabAt(2)!!.setIcon(R.drawable.ic_list_white_24dp)
-
-
-
     }
 }
---
+++ app/src/main/java/com/project/mygopek/data/Money.kt
@@ -0,0 +1,7 @@
+package com.project.mygopek.data
+
+data class Money(
+    var name: String,
+    var detail: String,
+    var photo: Int?
+)
