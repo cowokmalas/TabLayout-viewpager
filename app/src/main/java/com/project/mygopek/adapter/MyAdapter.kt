ff44aebfde05bf138e12fa8bef869814fb9a2c2d
cowokmalas <fahmyfauzii@gmail.com>
2020-05-10 14:42:55 +0700

    Update

--- app/src/main/java/com/project/mygopek/adapter/MyAdapter.kt
+++ app/src/main/java/com/project/mygopek/adapter/MyAdapter.kt
@@ -1,13 +1,12 @@
 package com.project.mygopek.adapter
 
+import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.project.mygopek.R
 import com.project.mygopek.fragment.FirstFragment
 import com.project.mygopek.fragment.LastFragment
 import com.project.mygopek.fragment.SeccondFragment
-import kotlinx.android.synthetic.main.activity_main.*
 
 @Suppress("DEPRECATION")
 class MyAdapter(fm: FragmentManager):FragmentPagerAdapter(fm){
--- app/src/main/java/com/project/mygopek/fragment/SeccondFragment.kt
+++ app/src/main/java/com/project/mygopek/fragment/SeccondFragment.kt
@@ -1,23 +1,27 @@
 package com.project.mygopek.fragment
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 import com.project.mygopek.R
+import com.project.mygopek.adapter.CardViewAdapter
+import com.project.mygopek.adapter.GridAdapter
+import com.project.mygopek.domain.DataMoney
 import com.project.mygopek.domain.DataMoney.listData
 import com.project.mygopek.domain.Money
 
 /**
  * A simple [Fragment] subclass.
  */
-class SeccondFragment : Fragment() {
+class SeccondFragment() : Fragment() {
     private lateinit var cardView:RecyclerView
     private val cardList:ArrayList<Money> = ArrayList()
 
@@ -26,13 +30,20 @@ class SeccondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         val view= inflater.inflate(R.layout.fragment_seccond, container, false)
-        cardView.layoutManager=LinearLayoutManager(this.activity)
-        cardList.addAll(listData)
+        cardView=view.findViewById(R.id.rv_cardview)
+        cardView.layoutManager=LinearLayoutManager(activity)
         cardView.setHasFixedSize(true)
-        cardView.adapter=com.project.mygopek.adapter.CardViewAdapter(cardList)
+        cardList.addAll(listData)
+       /* val cardViewAdapter= CardViewAdapter(cardList)
+          cardView.adapter=cardViewAdapter
+        */
+
         return view
     }
 
 
 }
+
+
